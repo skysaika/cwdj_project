262912a31b50d781169d8c7e8aa866d6d9a8cafc
saysuu <skysaika@yandex.ru>
2024-03-19 11:40:52 +0300

    add vlog: data.json, admin.py

--- vlog/admin.py
+++ vlog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from vlog.models import VlogPost
+
+
+@admin.register(VlogPost)
+class VlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'created', 'is_published')
+    list_filter = ('created', 'is_published')
